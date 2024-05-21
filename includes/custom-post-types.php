65b4748f45fb955e1b51a47d9c8edd3cf65a51fe
anthonyhorne <anthonyhorne@live.co.za>
2024-05-21 20:15:33 +0200

    Readd error message

--- includes/custom-post-types.php
+++ includes/custom-post-types.php
@@ -24,9 +24,9 @@ function upkeepify_register_custom_post_types() {
     if ($result) {
         // Add a success notification
         upkeepify_add_notification('Maintenance Tasks custom post type registered successfully.', 'success');
-    //} else {
+    } else {
         // Add an error notification
-    //    upkeepify_add_notification('Failed to register Maintenance Tasks custom post type.', 'error', array('additional_data' => 'value'), true);
+        upkeepify_add_notification('Failed to register Maintenance Tasks custom post type.', 'error', array('additional_data' => 'value'), true);
     }
 
 }
