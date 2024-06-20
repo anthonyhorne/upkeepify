28915cb55e63e58ea7c6e3184c3fd49253701128
Anthony Horne <anthonyhorne@live.co.za>
2024-06-20 20:21:58 +0000

    Remove Debug Messages

--- js/admin-settings.js
+++ js/admin-settings.js
@@ -1,28 +1,26 @@
 jQuery(document).ready(function($) {
     // Function to toggle SMTP settings visibility
     function toggleSmtpSettings() {
-        console.log('Toggling SMTP settings');
         $('.smtp_setting').toggle(!$('#upkeepify_smtp_option').is(':checked'));
     }
 
-    // Function to toggle the visibility of the "Thank You Service Provider Page" URL
-    function toggleThankYouPageUrl() {
-        console.log('Toggling Thank You Page URL');
+    // Function to toggle visibility based on the checkbox
+    function toggleThankYouPageSetting() {
+        // Check if the checkbox is checked
         var isChecked = $('#upkeepify_enable_thank_you_page').is(':checked');
-        console.log('Checkbox checked:', isChecked);
-        console.log($('.upkeepify_thank_you_page_url').length + " elements found.");
-        $('.upkeepify_thank_you_page_url').toggle(isChecked);
-    }    
+        // Toggle the visibility of the input field
+        $('.upkeepify_row.upkeepify_thank_you_page_url').toggle(isChecked);
+    }
 
     // Run on document ready to apply the correct initial state
     toggleSmtpSettings();
-    toggleThankYouPageUrl(); // Apply initial state for the thank you page URL setting
+    // Initial check to set correct visibility on page load
+    toggleThankYouPageSetting();
 
-    // Attach a change event listener to the checkboxes
+    // Attach a change event listener to the SMTP checkbox
     $('#upkeepify_smtp_option').change(toggleSmtpSettings);
-    $('#upkeepify_enable_thank_you_page').change(toggleThankYouPageUrl); // Listen for changes on the thank you page checkbox
 
-        // Attach a change event listener to the Thank You Page checkbox using delegated events
-        $(document).on('change', '#upkeepify_enable_thank_you_page', toggleThankYouPageUrl);
-
+    // Setup change event listener
+    $('#upkeepify_enable_thank_you_page').change(toggleThankYouPageSetting);
+    
 });
